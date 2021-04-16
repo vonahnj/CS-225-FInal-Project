9fc244f09231d55820d2f3052e8fe2876837b7ba
Sonali Merchia <sonalimerchia@yahoo.com>
2021-04-16 14:29:13 -0700

    finish linking

--- graph.cpp
+++ graph.cpp
@@ -9,7 +9,6 @@
 
 
 namespace encounters {
-    const size_t Graph::NUM_COLUMNS = 11;
 
     Graph::Graph(const std::string &file_name) {
         // open file
@@ -17,6 +16,9 @@ namespace encounters {
         fin.open(file_name, std::ios::in);
 
         int missedEntries = 0;
+        int wrongState = 0;
+        int linkTime = 0;
+        size_t withinTime = 0;
         while (fin.good()) {
             // Read in columns of line
             std::string line, word;
@@ -28,15 +30,16 @@ namespace encounters {
             }
 
             // Ensure there are enough columns in the line
-            if (components.size() < NUM_COLUMNS) {
+            if (components.size() < NUM_COLUMNS || components.at(STATE_COLUMN) != desired_state) {
+                wrongState++;
                 missedEntries++;
                 continue;
             }
 
             // parse columns to managable form
-            date d = parseDate(components.at(0));
-            double lat = parseDouble(components.at(9));
-            double longit = parseDouble(components.at(10));
+            date d = parseDate(components.at(DATE_COLUMN));
+            double lat = parseDouble(components.at(LATITUDE_COLUMN));
+            double longit = parseDouble(components.at(LONGITUDE_COLUMN));
 
             // add to list of nodes if values all exist
             if (d.year == 0 || lat == 0 || longit == 0) {
@@ -44,11 +47,27 @@ namespace encounters {
             } else {
                 encounter *newEncounter = new encounter(d, lat, longit);
                 nodes_.push_back(newEncounter);
+                
+                bool updated = false;
+                while (!updated && withinTime < nodes_.size() - 1) {
+                    if (dist(nodes_.at(withinTime)->time, nodes_.back()->time) > TIME_THRESHOLD ) {
+                        withinTime++;
+                    } else updated = true;
+                }
+
+                for (size_t index = withinTime; index < nodes_.size() - 1; index++) {
+                    linkTime++;
+                    linkTimewise(index, nodes_.size() - 1);
+                }
             }
         }
 
+        linkNodes();
+
         std::cout<<"missed: "<<missedEntries<<std::endl;
         std::cout<<"worked: "<<nodes_.size()<<std::endl;
+        std::cout<<"linked timewise: "<<linkTime<<std::endl;
+        std::cout<<"wrong state: "<<wrongState<<std::endl;
         std::cout<<"read: "<<(1 - (double)(missedEntries)/(missedEntries + nodes_.size()))<<std::endl;
     }
 
@@ -86,5 +105,38 @@ namespace encounters {
             return std::stod(doubleStr);
         } catch (std::invalid_argument e) {} catch (std::out_of_range e) {}
         return 0;
+    }
+
+    void Graph::linkNodes() {
+        int linkDist = 0;
+
+        for (size_t f_index = 0; f_index < nodes_.size(); f_index++) {
+            encounter &first = *nodes_.at(f_index);
+            bool unlinked = true;
+
+            for (size_t l_index = f_index + 1; l_index < nodes_.size(); l_index++) {
+                encounter &second = *nodes_.at(l_index);
+
+                if (dist(first, second) < DISTANCE_THRESHOLD) {
+                    linkDist++;
+                    unlinked = false;
+                }
+            }
+
+            if (unlinked) {
+                std::cout<<"unlinked @"<<*nodes_.at(f_index)<<std::endl;
+            }
+        }
+
+        std::cout<<"linked distance: "<<linkDist<<std::endl;
+    }
+
+    void Graph::linkTimewise(size_t firstIndex, size_t secondIndex) {
+        encounter *first = nodes_.at(firstIndex);
+        encounter *second = nodes_.at(secondIndex);
+        double timeDist = dist(first->time, second->time);
+
+        first->time_neighbors.push_back(encounter::edge(*first, *second, timeDist));
+        second->time_neighbors.push_back(encounter::edge(*second, *first, timeDist));
     }
 }
--- graph.h
+++ graph.h
@@ -5,6 +5,17 @@
 
 namespace encounters {
     class Graph {
+        static const size_t NUM_COLUMNS = 11;
+        static const size_t DATE_COLUMN = 0;
+        static const size_t STATE_COLUMN = 2;
+        static const size_t LATITUDE_COLUMN = 9;
+        static const size_t LONGITUDE_COLUMN = 10;
+
+        const std::string desired_state = "il";
+        const double DISTANCE_THRESHOLD = 10.0;
+        const double TIME_THRESHOLD = 7.0;
+        
+
         public: 
         Graph(const std::string &file_name);
         ~Graph();
@@ -14,5 +25,7 @@ namespace encounters {
 
         private:
         std::vector<encounter*> nodes_;
+        void linkNodes();
+        void linkTimewise(size_t firstIndex, size_t secondIndex);
     };
 }
