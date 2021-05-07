a44196d02eca45f5789070e42e5068e10e885d9a
jvonahn2 <jvonahn2@illinois.edu>
2021-05-06 23:35:32 -0700

    Fixed error in betweenness and put in mock data for testing purposes

--- graph.cpp
+++ graph.cpp
@@ -104,11 +104,21 @@ namespace encounters {
     vector<double> Graph::getBetweennessValues() {
         vector<double> values = vector<double>(nodes_.size(), 0.0);
         vector<int> tree;
+        // vector<vector<int> > test_trees; 
+        // vector<int> tree1 = {-1, 0, -2, 0};
+        // vector<int> tree2 = {1, -1, -2, 1};
+        // vector<int> tree3 = {-2, -2, -1, -2};
+        // vector<int> tree4 = {3, 3, -2, -1};
+        // test_trees.push_back(tree1); 
+        // test_trees.push_back(tree2); 
+        // test_trees.push_back(tree3); 
+        // test_trees.push_back(tree4); 
 
         for(int i = 0; i < (int)nodes_.size(); i++) {
             tree = getSpanningTreeDijk(i);
+            //tree = test_trees[i];
             for(int j = 0; j < (int)nodes_.size(); j++) {
-                if(tree[j] != -2 && tree[i] != -1) {
+                if(tree[j] != -2 && tree[j] != -1) {
                     int curr = j;
                     while(tree[curr] != -1) {
                         values[curr]++;
