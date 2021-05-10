41d53c72d3e97d087a1d252c74881595c85f4f73
jvonahn2 <jvonahn2@illinois.edu>
2021-05-10 04:43:53 -0700

    Added test for betweenness centrality

--- tests/test_betweenness.cpp
+++ tests/test_betweenness.cpp
@@ -7,35 +7,19 @@
 
 using namespace encounters;
 
-TEST_CASE("Check the results of the top N betweenness values") {
-    Graph graph("tests/test_data/large_data.csv");
+TEST_CASE("Verify small data values") {
+    Graph graph("tests/test_data/small_data.csv");
     vector<double> values = graph.getBetweennessValues();
+    //small data forms a loop between points 0, 1, and 3
+    //This means that each point in the loop is part of 2 shortest paths out of 6 possible, so a value of .33
+    vector<double> expected = {1.0/3.0, 1.0/3.0, 0, 1.0/3.0};
+    REQUIRE(values == expected);
+}
 
-    // pick N=10 top values to probe
-    int num = 10;
-    if(num > graph.getNodeCount()) {
-      return;
-    }
-
-    vector<double> highest = vector<double>(num, 0);
-    for(int i = 0; i < (int)values.size(); i++) {
-        if(values[i] > highest[num - 1]) {
-            for(int j = 0; j < num; j++) {
-                if(values[i] > highest[j]) {
-                    int curr = num - 1;
-                    while(curr > i) {
-                        highest[curr] = highest[curr - 1];
-                        curr--;
-                    }
-                    highest[j] = values[i];
-                    break;
-                }
-            }
-        }
-    }
-
-    // Print top N=10 highest values
-    for(int i = 0; i < num; i++) {
-        std::cout << highest[i] << std::endl;
-    }
+TEST_CASE("Verify Parallelogram data values") {
+    Graph graph("tests/test_data/parallelogram.csv");
+    vector<double> values = graph.getBetweennessValues();
+    //nodes 1 and 2 are the most central on the graph, which lines up with the centrality values
+    vector<double> expected = {1.0/3.0, 0.4, 7.0/15.0, 1.0/3.0, 1.0/3.0, 1.0/3.0};
+    REQUIRE(values == expected);
 }
